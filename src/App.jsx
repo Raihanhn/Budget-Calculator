0e199b4d619fc29663561b3c553124821957c1d5
raihanhn <raihanhn20@gmail.com>
2023-02-12 19:27:49 +0600

    4th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import Alert from './components/Alert'
+import ExpenseForm from './components/ExpenseForm'
+import ExpenseList from './components/ExpenseList'
 
 function App() {
 
   return (
-    <div className="App">
-      Hello Budget
-    </div>
+    <>
+      <Alert/>
+      <ExpenseForm/>
+      <ExpenseList/>
+    </>
   )
 }
 
--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Item from './ExpenseItem'
 
 const ExpenseList = () => {
   return (
-    <div>ExpenseList</div>
+    <div>ExpenseList
+      <Item/>
+    </div>
   )
 }
 
