2c9e311a3f2cf15ebbcafc57fc561cbacd8950b7
raihanhn <raihanhn20@gmail.com>
2023-02-14 22:14:35 +0600

    7th commit

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Item from './ExpenseItem'
 
-const ExpenseList = () => {
+const ExpenseList = ({expense, early}) => {
   return (
     <div>ExpenseList
       <Item/>
