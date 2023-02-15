571ba6d9352d54b10f3972f994be71ef147610af
raihanhn <raihanhn20@gmail.com>
2023-02-15 23:34:37 +0600

    6th commit

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Item from './ExpenseItem'
 
-const ExpenseList = ({expense, early}) => {
+const ExpenseList = ({expense}) => {
   return (
-    <div>ExpenseList
-      <Item/>
-    </div>
+    <>
+      <ul className="list">
+        {expense.map(expenses => {
+          return <Item key={expenses.id} expenses={expenses} />;
+        })}
+      </ul>
+      {expense.length > 0 && <button className='btn'> clear expense </button> }
+    </>
   )
 }
 
